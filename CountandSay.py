b2c2c581224e5c262174c201a9b7c53db7c8be6f
Arshad0786 <js07150715@gmail.com>
2020-05-17 23:03:52 +0800

    updating code that is accepted by leetcode and update unittest file

--- CountandSay.py
+++ CountandSay.py
@@ -1,3 +1,25 @@
 class Solution:
     def countAndSay(self, n):
-        pass
+        firstValue = "1"
+        if(n > 1):
+            return self.createcall(self.countAndSay(n-1))
+        else:
+            return firstValue
+
+
+
+    
+    def createcall(self,prev):
+        output = ""
+        count = 1
+        for i in range(len(prev)):
+            if(i==len(prev)-1):
+                output = output + str(count)
+                output = output + prev[i]
+                return output
+            if(prev[i]==prev[i+1]):
+                count = count + 1
+            else:
+                output = output + str(count)
+                output = output + prev[i]
+                count = 1
--- unit_test.py
+++ unit_test.py
@@ -3,10 +3,30 @@ from CountandSay import Solution
 
 
 class ImplementstrStrTest(unittest.TestCase):
-    def test_basic_function(self):
+    def test_basic_function1(self):
         temp = Solution()
         self.n = 1
-        self.assertEqual(temp.countAndSay(1), 1)
+        self.assertEqual(temp.countAndSay(self.n), "1")
+
+    def test_basic_function2(self):
+        temp = Solution()
+        self.n = 2
+        self.assertEqual(temp.countAndSay(self.n), "11")
+
+    def test_basic_function3(self):
+        temp = Solution()
+        self.n = 3
+        self.assertEqual(temp.countAndSay(self.n), "21")
+
+    def test_basic_function4(self):
+        temp = Solution()
+        self.n = 4
+        self.assertEqual(temp.countAndSay(self.n), "1211")
+
+    def test_basic_function5(self):
+        temp = Solution()
+        self.n = 5
+        self.assertEqual(temp.countAndSay(self.n), "111221")
 
 
 if __name__ == "__main__":
